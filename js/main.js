65c2b5c45ce490816aeb8958ce3a584a43d52120
Pavlo-Zibrov-Vork <gfdsa@outlook.com>
2016-11-04 11:35:44 +0200

    Menu cetered

--- js/main.js
+++ js/main.js
@@ -12,7 +12,10 @@ $(document).ready(function() {
         speed: 1000,
         paginationBulletRender: function (index, className) {
             return '<span class="swiper-pagination-bullet"> 0' + (className + 1) + '.</span>';
-        }
+        },
+        onlyExternal: true
+
+
     });
     
     //calculate when sticky menu should appear
@@ -27,13 +30,15 @@ $(window).on("resize", function(){
     $('#sticky').data('bs.affix').options.offset = $(window).height();
 });
 
+//Show or hide slider menu on desktop
 $(window).scroll(function() {
-    if ($(this).scrollTop() > 80) { //use `this`, not `document`
-        $('.nav-slider').fadeOut( "slow" );
-    } else {
-        $('.nav-slider').fadeIn( "slow" );
+    if ($(window).width() > 1359){
+        if ($(this).scrollTop() > 80) {
+            $('.nav-slider').fadeOut( "slow" );
+        } else {
+            $('.nav-slider').fadeIn( "slow" );
+        }
     }
 });
-
 
 
